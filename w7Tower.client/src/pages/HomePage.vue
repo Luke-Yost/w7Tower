f78b6e6518485c68f454e42cf8f6ba806a14e1ed
Luke Yost <lukexyost@gmail.com>
2022-06-25 15:28:54 -0600

    filterEvents working!!!!

--- w7Tower.client/src/pages/HomePage.vue
+++ w7Tower.client/src/pages/HomePage.vue
@@ -3,10 +3,18 @@
     <div class="row">
       <div class="col-10 offset-1">
         <div class="row">
-          <div class="col-12 m-3 p-2">
-            <div class=" border border-3 border-info rounded bg-dark text-center">
-              <h3>Get tickets to events here at Tower Event. We just might be the best website ever made. Period.</h3>
-            </div>
+          <div class="col-12 m-2 p-1  border border-3 border-info rounded bg-dark text-center">
+            <h3>Get tickets to events here at Tower Event. We just might be the best website ever made. Period.</h3>
+          </div>
+          <div class="col-md-8 offset-md-2 bg-light border rounded shadow d-flex justify-content-center px-1 py-2">
+            <h6>Filter Events By Type</h6>
+          </div>
+          <div class="col-md-8 offset-md-2 bg-light border rounded shadow d-flex justify-content-evenly px-1 py-2">
+              <button @click="filterEvent = ''" class="btn btn-info">All Events</button>
+              <button  @click="filterEvent = 'concert'" class="btn btn-info">Concerts</button>
+              <button @click="filterEvent = 'sport'" class="btn btn-info">Sporting Events</button>
+              <button @click="filterEvent = 'digital'" class="btn btn-info">Digital Events</button>
+              <button @click="filterEvent = 'convention'" class="btn btn-info">Conventions</button>
           </div>
         </div>
         <div class="row" v-for="e in events" :key="e.id">
@@ -19,14 +27,25 @@
 
 <script>
 import { AppState } from "../AppState"
-import { computed, onMounted } from "vue"
+import { computed, onMounted, ref, watchEffect } from "vue"
 import { logger } from "../utils/Logger"
 import Pop from '../utils/Pop'
 import Event from "../components/Event.vue"
 import { eventsService } from "../services/EventsService"
+import { useRoute } from "vue-router"
 export default {
     name: "Home",
     setup() {
+        const route = useRoute()
+        const filterEvent = ref('')
+        watchEffect(async () => {
+          try {
+            await eventsService.getEvents(filterEvent.value)
+          } catch (error) {
+            logger.error(error)
+            Pop.toast(error.message, 'error')
+          }
+        })
         onMounted(async () => {
             try {
                 await eventsService.getEvents();
@@ -37,7 +56,8 @@ export default {
             }
         });
         return {
-            events: computed(() => AppState.events)
+          filterEvent,
+            events: computed(() => AppState.events.filter( e => filterEvent.value ? e.type == filterEvent.value : true))
         };
     },
     components: { Event }
