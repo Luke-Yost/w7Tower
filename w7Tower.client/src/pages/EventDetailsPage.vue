c58868dd478230b2602e9a0650a799c836777a01
Luke Yost <lukexyost@gmail.com>
2022-06-27 08:40:20 -0600

    dd

--- w7Tower.client/src/pages/EventDetailsPage.vue
+++ w7Tower.client/src/pages/EventDetailsPage.vue
@@ -4,6 +4,27 @@
       <div class="col-12 offset-1 d-flex justify-content-center">
         <h2 class="m-3 border border-2 border-dark text-info rounded p-2 bg-light shadow">{{activeEvent.name}}</h2>
       </div>
+      <div class="col-12">
+        <div class="row border border-2 border-dark text-dark rounded bg-light shadow d-flex">
+          <div class="col-12 d-flex justify-content-center border  m-1">
+            <img class="justify-content-center m-1 rounded" :src="activeEvent.coverImg" alt="event image">
+          </div>
+          <div class="col-12 border border-top-3 border-dark">
+            <p class="fs-5">{{activeEvent.description}}</p>
+          </div>
+          <div class="col-md-6 d-flex flex-column align-content-center fs-4">
+            <p>{{activeEvent.location}}</p>
+            <p>Available Tickets{{activeEvent.capacity}}</p>
+            <p>{{activeEvent.startDate}}</p>
+          </div>
+          <div class="col-md-6 d-flex flex-column align-content-center fs-4">
+            <p>Hosted By {{activeEvent.creator.name}}</p>
+            <p>Event Type: {{activeEvent.type}}</p>
+
+          </div>
+        </div>
+      </div>
+      
       </div>
     </div>
 </template>
@@ -29,5 +50,8 @@ export default {
 
 
 <style lang="scss" scoped>
-
+img{
+  max-width:fit-content ;
+  max-height: 400px;
+}
 </style>
